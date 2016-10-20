c8f75957bc4d6cd5c15e1f7095a8ef8084645495
Brendon <brendon.verissimo@gmail.com>
2016-10-20 16:23:28 -0700

    Check for duplicates in server data response on Messages.index and remove

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -6,20 +6,29 @@ angular.module("webchat")
         var pollerId;
 
         $scope.user = User.user();
-
         $scope.input = {
             value: ""
         };
-
-        $scope.messages = [
-        ];
+        $scope.messages = [];
+        $scope.sentMessages = {};
 
         function startPoller() {
             pollerId = Poller.poll(10* 1000, function (data) {
                 if (data.length) {
+                    //Check sentMessages object keys
+                    if (Object.keys($scope.sentMessages).length) {
+                        console.log("SENT MESSAGES");
+                        for (var i = 0; i < data.length; i++) {
+                            if ($scope.sentMessages.hasOwnProperty(data[i]._id)) {
+                                console.log("DUPLICATE");
+                                //Remove message from sent
+                                delete $scope.sentMessages[data[i]._id];
+                                //Remove duplicate
+                                data.splice(i, 1);
+                            }
+                        }
+                    }
                     $scope.messages = $scope.messages.concat(data);
-                    console.log("SET lastReceivedMessage", data[data.length -1].created);
-                    Poller.lastReceivedMessage(data[data.length -1].created);
                 }
             });
         }
@@ -31,10 +40,17 @@ angular.module("webchat")
         $scope.sendMessage = function() {
             console.log("SEND: ", $scope.input.value)
             var userId = $scope.user && $scope.user._id;
+            var time = Date.now();
             if (userId === undefined) return;
-            MessageResource.create({content:$scope.input.value, _id: userId}).$promise.then(function(data) {
-                console.log("MESSAGE CREATE: ", data);
+            
+            MessageResource.create({content:$scope.input.value, _id: userId, created: time}).$promise.then(function(data) {
                 $scope.input.value = "";
+                var message = data.message;
+                if (!message) return; //TODO: handle error
+                message.creator = $scope.user;
+                $scope.messages.push(message);
+                $scope.sentMessages[message._id] = 1;
+                console.log("MESSAGE CREATED AT: ", message.created);
             }).catch(function(error) {
                 //TODO: Show error dialog
                 console.log("ERROR: main.sendMessage", error);
