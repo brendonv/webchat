0a496c70bc3effae00c2d232b183024c5440f779
Brendon <brendon.verissimo@gmail.com>
2016-10-19 18:26:58 -0700

    Setup karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,68 +1,36 @@
 // Karma configuration
 // Generated on Wed Oct 19 2016 14:14:46 GMT-0700 (PDT)
-
 module.exports = function(config) {
   config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+    basePath: './',
 
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
-
-
-    // list of files / patterns to load in the browser
     files: [
+      'vendor/angular/angular.js',
+      'vendor/angular-route/angular-route.js',
+      'vendor/jquery/dist/jquery.js',
+      'vendor/angular-mocks/angular-mocks.js',
+      'vendor/angular-resource/angular-resource.js',
+      'app/**/*.js'
     ],
 
+    autoWatch: true,
 
-    // list of files to exclude
-    exclude: [
-    ],
+    frameworks: ['jasmine'],
 
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
 
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-jasmine',
+      'karma-junit-reporter'
+    ],
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    junitReporter: {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
 
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
-}
+  });
+};
