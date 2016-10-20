6ef66eaae293dc25f2a89b302b23a2d87eff236c
Brendon <brendon.verissimo@gmail.com>
2016-10-20 10:11:48 -0700

    Add messages stub; Establish long polling for messages; Message send implementation

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -1,5 +1,5 @@
 angular.module("webchat")
-    .controller("MainCtrl", ["$scope", function($scope) {
+    .controller("MainCtrl", ["$scope", "MessageResource", "Poller", function($scope, MessageResource, Poller) {
 
         console.log("MAIN PAGER");
 
@@ -7,12 +7,31 @@ angular.module("webchat")
             value: ""
         };
 
+        $scope.messages = [
+            "messageOne",
+            "messageTwo",
+            "messageThree",
+            "messageFour",
+            "messageFive"
+        ];
+
+        Poller.poll(10* 1000, function (data) {
+            messages.append(data);
+        })
+
         $scope.userInput = function() {
-            console.log("USER INPUT", $scope.input.value);
+            console.log($scope.input.value);
         };
 
         $scope.sendMessage = function() {
-
+            console.log("SEND: ", $scope.input.value)
+            MessageResource.create({content:$scope.input.value}).$promise.then(function(data) {
+                console.log("MESSAGE CREATE: ", data);
+            }).catch(function(error) {
+                console.log("ERROR: main.sendMessage", error);
+            });
         }
+
+
 
     }]);
