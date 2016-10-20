276c09681c181f6b4fa60604f1f993ed7ad2c902
Brendon <brendon.verissimo@gmail.com>
2016-10-20 10:12:28 -0700

    Signup implementation

--- app/services/user.js
+++ app/services/user.js
@@ -7,14 +7,25 @@ angular.module('webchat')
         };
         return $resource(url, params, methods);
     }])
-    .factory('User', ['$rootScope', 'UserResource', 'store', function($rootScope, UserResource, store) {
-
+    .factory('User', ['$rootScope', 'UserResource', function($rootScope, UserResource) {
+        var user;
         return {
-            signup: signup
+            signup: signup,
+            user: getUser
         };
 
         function signup(username) {
+            UserResource.signup({username: username}).$promise.then(function(data) {
+                user = data.user;
+                console.log("USER: ", user);
+                $rootScope.$emit("SIGNEDUP");
+            }).catch(function(error) {
+                console.log("ERROR: user.signup ", error);
+            });
+        }
 
+        function getUser() {
+            return user;
         }
 
     }]);
